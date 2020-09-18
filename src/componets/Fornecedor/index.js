e67d7c535cc40f4d5118de751f88e0e5e70c9476
vagnersampaio <vagnersampaio92@gmail.com>
2020-09-18 16:01:42 -0300

    fornecedores integrado

--- src/componets/Fornecedor/index.js
+++ src/componets/Fornecedor/index.js
@@ -1,60 +1,88 @@
 import React, { useState, useEffect, useContext, } from "react";
 import { Container, Titulo, Card, Box, Busca, Boxd, Alinha } from "./styles";
 import TextField from '@material-ui/core/TextField'
+import api from '../../services/api'
 
 const Homeadm = () => {
-    const [detalhe, Setdetalhe] = useState('');
-    console.log(detalhe)
+    const [detalhe, Setdetalhe] = useState({});
+    const [emailstate, Setemail] = useState('');
+    const [Usuarios, SetUsuarios] = useState([{}]);
+
+
+    useEffect(() => {
+        carrega()
+    }, []);
+
+    const carrega = async () => {
+        const resposta = await api.get('listafornecedoradm')
+        console.log(resposta.data)
+        SetUsuarios(resposta.data)
+    }
+    const deleta = async (id) => {
+        // const resposta = await api.delete('excluiusuario/'+id)
+        carrega()
+        Setdetalhe({ ...detalhe, ["name"]: undefined})
+    }
+    const plano = async (id) => {
+        let data={}
+        data.id=id
+        data.name='2'
+        const resposta = await api.put('editaplano',data)
+        carrega()
+        Setdetalhe({ ...detalhe, ["name"]: undefined})
+    }
+
+    const email = async () => {
+
+
+        for(let x=0; x<Usuarios.length; x++){
+            if(Usuarios[x].email == emailstate){
+              
+                SetUsuarios([Usuarios[x]])
+            }
+
+        }
+       
+    }
+    
+    console.log(Usuarios)
+    // onChange={e => { Setperfil({ ...perfil, ["name"]: e.target.value }) }}
+
     return (
         <Container>
-            {detalhe == '' ?
+            {detalhe.name == undefined ?
                 <>
                     <Titulo>Fornecedores cadastrados na plataforma</Titulo>
                     <Busca>
                         {/* <p>Pesquisa usuário por email</p> */}
-
-                        <TextField id="standard-basic" label="Pesquisa por email" />
-                        <button >Pesquisar</button>
+                        <TextField onChange={e => {Setemail(e.target.value)}} id="standard-basic" label="Pesquisa por email" />
+                        <button onClick={email}>Pesquisar</button>
                     </Busca>
                     <Box>
-                        <Card>
-                            <p>João Paulo</p>
-                            <button onClick={e => { Setdetalhe('DSD') }}>Ver mais</button>
-                        </Card>
-                        <Card>
-                            <p>João Paulo</p>
-                            <button>Ver mais</button>
-                        </Card>
-                        <Card>
-                            <p>João Paulo</p>
-                            <button>Ver mais</button>
-                        </Card>
-                        <Card>
-                            <p>João Paulo</p>
-                            <button>Ver mais</button>
-                        </Card>
-                        <Card>
-                            <p>João Paulo</p>
-                            <button>Ver mais</button>
-                        </Card>
-                        <Card>
-                            <p>João Paulo</p>
-                            <button>Ver mais</button>
-                        </Card>
+                        <>
+                            {Usuarios.map((u) => (
+                                <>
+                                    <Card>
+                                        <p>{u.name}</p>
+                                        <button onClick={e => { Setdetalhe(u) }}>Ver mais</button>
+                                    </Card>
+                                </>
+                            ))}
 
+                        </>
 
                     </Box>
                 </> : <Alinha>
                     <Boxd>
-                        <p>João Paulo</p>
-                        <p>gbrito.one@gmail.com</p>
-                        <p>Pelotas</p>
-                        <p>(53) 9 8100-8667</p>
-                        <div>
-                            <button onClick={e => { }}>Deletar</button>
-                            <button onClick={e => { }}>Dar plano</button>
-                        </div>
-
+                        <p>{detalhe.name}</p>
+                        <p>{detalhe.email}</p>
+                        <p>{detalhe.cidade}</p>
+                        <p>{detalhe.phone}</p>
+                         <div>
+                             <button onClick={()=>{deleta(detalhe.id)}}>Deletar</button>
+                             <button onClick={e => {plano(detalhe.id) }}>Dar plano</button>
+                         </div>
+                       
                     </Boxd>
                 </Alinha>
 
@@ -67,5 +95,26 @@ const Homeadm = () => {
 
 export default Homeadm;
 
+
+
+
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        // <div>
+                        //     <button onClick={e => { }}>Deletar</button>
+                        //     <button onClick={e => { }}>Dar plano</button>
+                        // </div>
 
 
