8a7fa627df0ff8c88b0954d339b4e12093c5e039
vagnersampaio <vagnersampaio92@gmail.com>
2020-09-17 23:05:56 -0300

    responsividade na home adm

--- src/componets/Homeadm/styles.js
+++ src/componets/Homeadm/styles.js
@@ -4,6 +4,8 @@ export const Container = styled.div`
 margin-top:30px;
 padding: 10px 40px;
 width:100%;
+word-wrap: break-word;
+
 `;
 
 export const Titulo = styled.div`
@@ -27,14 +29,24 @@ export const Box = styled.div`
 display: flex;
 width:100%;
 justify-content: space-between;
-
+flex-flow:row wrap;
+@media (max-width: 402px) {
+    justify-content:center;
+    width:80%;
+  margin-left:0px;
+     
+}
 `;
 export const Card = styled.div`
 border: 1px solid #FF522C;
 box-sizing: border-box;
 border-radius: 18px;
 padding:15px 25px 25px 25px;
-
+@media (max-width: 724px) {
+    margin-top: 30px;
+    padding:5px 15px 15px 15px;
+     
+}
     h4{
         font-style: normal;
         font-weight: 600;
@@ -51,6 +63,11 @@ padding:15px 25px 25px 25px;
 
         text-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
         margin-bottom:15px;
+        @media (max-width: 724px) {
+            h4{
+            font-size: 8x  !important;
+            }
+        }
 
     }
     p{
@@ -67,6 +84,9 @@ padding:15px 25px 25px 25px;
         color: #666666;
 
         text-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+        @media (max-width: 724px) {
+          p{font-size: 8x !important;}  
+        }
     }
     span{
         font-style: normal;
@@ -82,5 +102,11 @@ padding:15px 25px 25px 25px;
         color: #666666;
 
         text-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+        @media (max-width: 724px) {
+            span{
+                font-size: 14x !important;
+            }
+            
+        }
     }
 `;
