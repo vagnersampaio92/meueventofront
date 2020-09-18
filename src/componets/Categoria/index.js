f93a11421b8d40faa3ca90549a3582b912d1ba5f
vagnersampaio <vagnersampaio92@gmail.com>
2020-09-18 17:03:11 -0300

    deleta categoria do  evento

--- src/componets/Categoria/index.js
+++ src/componets/Categoria/index.js
@@ -1,47 +1,60 @@
 import React, { useState, useEffect, useContext, } from "react";
 import { Container, Titulo, Card, Box, Busca, Boxd } from "./styles";
 import TextField from '@material-ui/core/TextField'
+import api from '../../services/api'
 
 const Homeadm = () => {
-    const [detalhe, Setdetalhe] = useState('');
+    const [detalhe, Setdetalhe] = useState([{}]);
+
+    useEffect(() => {
+        carrega()
+    }, []);
+    // http://localhost:3001/api/excluicategorias/:id
+    const carrega = async () => {
+        const resposta = await api.get('listacategorias')
+        console.log(resposta.data)
+        Setdetalhe(resposta.data)
+    }
+    const exclui = async (id) => {
+        const resposta = await api.delete('excluicategorias/'+id)
+        console.log(resposta.data)
+        carrega()
+    }
+    const add = async (id) => {
+        let data={}
+        const resposta = await api.delete('excluicategorias',data)
+        console.log(resposta.data)
+        carrega()
+    }
     console.log(detalhe)
     return (
         <Container>
-                    <Titulo>Cadastrar nova categoria</Titulo>
-                    <Busca>
+            <Titulo>Cadastrar nova categoria</Titulo>
+            <Busca>
 
 
-                        <TextField id="standard-basic" label="Nova categoria" />
-                        <button >Salvar</button>
-                    </Busca>
-                    <Box>
-                        <Card>
-                            <p>Fotográfo</p>
-                            <button onClick={e => {  }}>Excluir </button>
-                        </Card>
-                        <Card>
-                            <p>Decoração</p>
-                            <button>Excluir</button>
-                        </Card>
-                        <Card>
-                            <p>Cerimonialista</p>
-                            <button>Excluir</button>
-                        </Card>
-                        <Card>
-                            <p>Animador de eventos</p>
-                            <button>Excluir</button>
-                        </Card>
-                        <Card>
-                            <p>Video Maker</p>
-                            <button>Excluir</button>
-                        </Card>
-                        <Card>
-                            <p>Buffet</p>
-                            <button>Excluir</button>
-                        </Card>
+                <TextField id="standard-basic" label="Nova categoria" />
+                <button >Salvar</button>
+            </Busca>
+            <Box>
+                
 
-                    </Box>
-        
+                <>
+               
+                    {detalhe.map((u) => (
+                        <>
+                            <Card>
+                                <p>{u.name}</p>
+                                <button onClick={e => {exclui(u.id)}}>Excluir</button>
+                            </Card>
+                        </>
+                    ))}
+
+                </>
+
+
+            </Box>
+
         </Container>
     );
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 const api = axios.create(
     {
-        baseURL: process.env.REACT_APP_API_URL
+        // baseURL: process.env.REACT_APP_API_URL
         
-        // baseURL:' http://localhost:3001/api/'
+        baseURL:' http://localhost:3001/api/'
         // baseURL:'https://boradeliveryone.herokuapp.com/api/'
     }
 )
